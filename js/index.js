5dc25608d21c8033fab95ae4d72d8ffdcb230a99
李德永 <lideyong@newcapec.net>
2020-12-16 18:24:55 +0800

    添加bootstrap样式、背景图、看板娘

---
+++ js/index.js
@@ -0,0 +1,10 @@
+<div id="loading">
+    <div class="spinner-box">
+        <div class="blue-orbit leo"></div>
+        <div class="green-orbit leo"></div>
+        <div class="red-orbit leo"></div>
+        <div class="white-orbit w1 leo"></div>
+        <div class="white-orbit w2 leo"></div>
+        <div class="white-orbit w3 leo"></div>
+    </div>
+</div>
